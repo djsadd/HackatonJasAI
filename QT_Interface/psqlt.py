8f26330b6ce39d81f1577aad8093d0d65a028d9c
Erasil <erasil.kazah222@gmail.com>
2023-04-08 17:25:45 +0600

    add connect to db

---
+++ QT_Interface/psqlt.py
@@ -0,0 +1,18 @@
+
+import psycopg2
+
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port
+        )
+    except Exception as e:
+        print(e)
+
+    return connection
